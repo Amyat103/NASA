f8647c25afc9ce4293c6aa18cdcc6665da062a1f
Amyat103 <zhoudavid7312@gmail.com>
2023-09-30 00:35:14 -0400

    added requests & json, io to convert image, PIL to open image

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import requests
 import key
+import json
+from PIL import Image
+from io import BytesIO
 
 r = requests.get(f"https://api.nasa.gov/planetary/apod?api_key={key.API_KEY}")
 print(r.status_code)
+
+response = json.loads(r.content)
+print(response)
+
+pic_request = requests.get(response["hdurl"])
+pic_byte = BytesIO(pic_request.content)
+pic_open = Image.open(pic_byte)
+pic_open.show()
