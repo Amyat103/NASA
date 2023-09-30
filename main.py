f4bbf8a527cb7fc9cc399fd1911fe87861db5b8c
Amyat103 <zhoudavid7312@gmail.com>
2023-09-30 00:55:45 -0400

    added input, if function, and parameters. if function to separate the requests

--- main.py
+++ main.py
@@ -4,12 +4,26 @@ import json
 from PIL import Image
 from io import BytesIO
 
-r = requests.get(f"https://api.nasa.gov/planetary/apod?api_key={key.API_KEY}")
-print(r.status_code)
+#Astronomy Picture of the Day (APOD)
 
-response = json.loads(r.content)
-print(response)
+date_param = {
+    "date": "2022-10-30"
+}
 
-pic_request = requests.get(response["hdurl"])
-pic_conv = Image.open(BytesIO(pic_request.content))
-pic_conv.show()
+count_param = {
+    "count": 3
+}
+
+choice = input("params")
+
+if choice == "date":
+    r = requests.get(f"https://api.nasa.gov/planetary/apod?api_key={key.API_KEY}", params=date_param)
+    print(r.status_code)
+    response = json.loads(r.content)
+    pic_request = requests.get(response["hdurl"])
+    pic_conv = Image.open(BytesIO(pic_request.content))
+    pic_conv.show()
+elif choice == "count":
+    r = requests.get(f"https://api.nasa.gov/planetary/apod?api_key={key.API_KEY}", params=count_param)
+    print(r.status_code)
+    response = json.loads(r.content)
